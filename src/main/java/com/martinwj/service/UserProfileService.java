aa792e26501c3de58aa6544e55bc3228018e2620
martin wj <wj2856379842@163.com>
2020-12-22 14:14:25 +0800

    七牛云对象存储更新

--- src/main/java/com/martinwj/service/UserProfileService.java
+++ src/main/java/com/martinwj/service/UserProfileService.java
@@ -1,5 +1,7 @@
 package com.martinwj.service;
 
+import com.martinwj.entity.UserProfile;
+
 /**
  * @ClassName: UserProfileService
  * @Description: TODO
@@ -7,4 +9,9 @@ package com.martinwj.service;
  * @createDate: 2020-12-21
  */
 public interface UserProfileService {
+    /**
+     * 保存用户扩展信息
+     * @param userProfile
+     */
+    public void save(UserProfile userProfile);
 }
--- src/main/java/com/martinwj/service/impl/UserProfileServiceImpl.java
+++ src/main/java/com/martinwj/service/impl/UserProfileServiceImpl.java
@@ -1,6 +1,9 @@
 package com.martinwj.service.impl;
 
+import com.martinwj.dao.userProfile.IUserProfileDAO;
+import com.martinwj.entity.UserProfile;
 import com.martinwj.service.UserProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,4 +15,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserProfileServiceImpl implements UserProfileService {
 
+    @Autowired
+    private IUserProfileDAO iUserProfileDAO;
+
+    /**
+     * 保存用户扩展信息
+     * @param userProfile
+     */
+    public void save(UserProfile userProfile) {
+        iUserProfileDAO.update(userProfile);
+    }
 }
